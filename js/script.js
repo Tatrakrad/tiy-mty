d8d39b97e0a4e0513534a9a52136bbbe29357c0b
Tatrakrad <riddick.taylor@gmail.com>
2016-12-09 09:46:39 -0600

    animates in one direction

--- js/script.js
+++ js/script.js
@@ -2,33 +2,41 @@
 
   console.log("now time for a script to occur");
 
+  //Element containing our changing text
   var animatedText = document.querySelector('.example');
-  animatedText.textConent = "";
-  console.log(animatedText);
 
+  //Possible messages to animate;
   var sampleStrings = ["by a Wizard","by two Wizards","by two children wearing a trenchcoat disguised as an Adult","by a fat pony","by a Vaporwave Space Princess","by Slavoj Žižek","by a Talking Car"];
-
+  //current selected message index range
   var select = 0;
-  var selectMax = sampleStrings.length-1;
+  var selectMax = sampleStrings.length;
 
-  console.log(select);
-  console.log(selectMax);
+  //animation speeds
+  var timeoutForSwitch = 2000;
+  var animatorSpeed = 150;
+
+  //start the reactor
+  setAnimatedPhrase();
 
   function animateString(aString,location){
-
+    console.log('animate a string',aString);
+    //clear
+    location.textContent = "";
+    //
     var i = 0;
     var animTyping = setInterval(function(){
-      location.textConent += aString.charAt(i);
+      location.textContent += aString.charAt(i);
       i++;
       if (i>aString.length){
         clearInterval(animTyping);
+        setTimeout(setAnimatedPhrase,timeoutForSwitch);
       }
-    },200);
+    },animatorSpeed);
   }
 
   function setAnimatedPhrase(){
-
-    if (select <= selectMax){
+    console.log("switchphrase",sampleStrings[select]);
+    if (select < selectMax){
       animateString(sampleStrings[select],animatedText);
     }else{
       select = 0;
@@ -37,7 +45,6 @@
     select++;
   }
 
- setInterval(setAnimatedPhrase, 2000);
 
 
 
